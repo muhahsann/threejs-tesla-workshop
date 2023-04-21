246672b1edd1c82dada0fba35734b2c5138be45b
Muhammad <Muhammad.Ahsan@staples.com>
2023-04-21 15:11:06 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useRef, useEffect } from "react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+const Orbit = () => {
+  const { camera, gl } = useThree();
+  useEffect(() => {
+    const controls = new OrbitControls(camera, gl.domElement);
+    return () => {
+      controls.dispose();
+    };
+  }, [camera, gl]);
+};
+
+const Box = (props) => {
+  const ref = useRef();
+
+  useFrame((state) => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+  });
+  return (
+    <mesh ref={ref} {...props}>
+      <boxBufferGeometry />
+      <meshBasicMaterial color="red" />
+    </mesh>
+  );
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Canvas style={{ background: "black" }} camera={{ position: [3, 3, 3] }}>
+        <Box position={[1,1,0]}/>
+        <Orbit />
+        <axesHelper args={[5]} />
+      </Canvas>
     </div>
   );
 }
