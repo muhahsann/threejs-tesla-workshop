0e0a1553bc66da130eb51efd748cccbac2c5106f
Muhammad <Muhammad.Ahsan@staples.com>
2023-05-10 18:31:14 +0500

    Add shows to models

--- src/App.js
+++ src/App.js
@@ -28,6 +28,14 @@ function App() {
         <Orbit setOrbitControls={setOrbitControls} />
         <CameraControls orbitControls={orbitControls} />
         <ambientLight intensity={0.2} />
+        <directionalLight
+          position={[6, 3, 0]}
+          intensity={2}
+          castShadow
+          shadow-mapSize-height={2 ** 10}
+          shadow-mapSize-width={2 ** 10}
+          shadow-radius={10}
+        />
         <axesHelper args={[5]} />
         <Bulb position={[-6, 3, 0]} />
         <Bulb position={[0, 3, 0]} />
